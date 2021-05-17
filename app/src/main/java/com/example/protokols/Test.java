1ffe4402c06c5df2fb1b2f4659edaf35cf882ed7
Maxim Zadneprovsky <maksysha40@gmail.com>
2021-05-17 12:59:51 +0300

    bottomnavigation

--- app/src/main/java/com/example/protokols/Test.java
+++ app/src/main/java/com/example/protokols/Test.java
@@ -6,10 +6,17 @@ import android.os.PersistableBundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class Test extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.silovoy_trans);
+
+        bottomNavigationView = findViewById(R.id.bottomNavView);
+        bottomNavigationView.setBackground(null);
     }
 }
