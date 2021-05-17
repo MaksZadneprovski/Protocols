8bf7396eb77381af980bbffb0047616d0e8855dd
Maxim Zadneprovsky <maksysha40@gmail.com>
2021-05-17 10:56:50 +0300

    layout

---
+++ app/src/main/java/com/example/protokols/Test.java
@@ -0,0 +1,15 @@
+package com.example.protokols;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Test extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.silovoy_trans);
+    }
+}
