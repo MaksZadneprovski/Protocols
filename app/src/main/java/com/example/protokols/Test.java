3e5be16c852d00ae8a3e88d1f02a46d2bed3f436
Maxim Zadneprovsky <maksysha40@gmail.com>
2021-05-18 11:45:54 +0300

    siltrans

--- app/src/main/java/com/example/protokols/Test.java
+++ app/src/main/java/com/example/protokols/Test.java
@@ -5,11 +5,13 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.textfield.TextInputEditText;
@@ -18,6 +20,8 @@ import com.google.android.material.textfield.TextInputLayout;
 public class Test extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
     private TextInputEditText et;
+    private ConstraintLayout cLObject;
+    private ConstraintLayout cLMeterage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +29,8 @@ public class Test extends AppCompatActivity {
         setContentView(R.layout.silovoy_trans);
 
         bottomNavigationView = findViewById(R.id.bottomNavView);
+        cLObject = findViewById(R.id.ConstrLayoutObject);
+
         bottomNavigationView.setBackground(null);
         bottomNavigationView.getMenu().getItem(2).setEnabled(false);
 
@@ -34,6 +40,14 @@ public class Test extends AppCompatActivity {
                 if (item.getItemId() == R.id.bottomNavIc1){
                     Intent i = new Intent(Test.this, ViewingProtokolsList.class);
                     startActivity(i);
+                }
+
+                else  if (item.getItemId() == R.id.bottomNavIc3){
+                    cLObject.setVisibility(View.VISIBLE);
+                }
+
+                else  if (item.getItemId() == R.id.bottomNavIc4){
+                    cLObject.setVisibility(View.GONE);
                 }
                 return true;
             }
